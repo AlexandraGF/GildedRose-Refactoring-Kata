91c05e209576a23ce5afd1d8fa5b5f55ee12fafa
Alexy <alexandra.fina@yahoo.com>
2018-02-27 12:50:53 +0000

    Test that update_quality works - pass

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,10 +1,16 @@
 require './gilded_rose.rb'
 
 describe GildedRose do
-
   it 'check that class respond to update_quality method' do
-    items = Item.new("Aged Brie", 20, 15)
+    items = Item.new('Aged Brie', 20, 15)
     gilded_rose = GildedRose.new(items)
     expect(gilded_rose).to respond_to(:update_quality)
+  end
+
+  it 'check that update_quality method is working' do
+    items = Item.new('Aged Brie', 20, 15)
+    items2 = Item.new('Aged Brie', 15, 10)
+    gilded_rose = GildedRose.new([items,items2])
+    expect((gilded_rose.update_quality)[0].sell_in).to eq(19)
   end
 end
