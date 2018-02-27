d21bb35df4abd0db58c532253cd3a3ca77e7ec10
Alexy <alexandra.fina@yahoo.com>
2018-02-27 16:34:07 +0000

    Tests (case edges)

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,35 +3,58 @@ require 'web_helpers.rb'
 
 describe GildedRose do
 
-  it 'check that class respond to update_quality method' do
-    expect(gilded_rose).to respond_to(:update_quality)
+  describe 'Basic tests: ' do
+    it 'check that class respond to update_quality method' do
+      expect(gilded_rose).to respond_to(:update_quality)
+    end
+
+    it 'check that update_quality method is working' do
+      expect((gilded_rose.update_quality)[0].sell_in).to eq(19)
+    end
+
+    it 'check that sell_in date does not increase' do
+      expect((gilded_rose.update_quality)[1].sell_in).not_to eq(16)
+    end
+
+    it 'check that quality value equals the right value' do
+      expect((gilded_rose.update_quality)[0].quality).to eq(16)
+    end
+
+    it 'check that quality value equals the right value(second example)' do
+      expect((gilded_rose2.update_quality)[0].quality).to eq(15)
+    end
+
+    it 'check that quality value does not equal the wrong value' do
+      expect((gilded_rose.update_quality)[0].quality).not_to eq(15)
+    end
+
+    it 'check that quality value does not go more than 50' do
+      expect((gilded_rose3.update_quality)[0].quality).to eq(50)
+    end
+
+    it 'check that quality value does not go negative' do
+      expect((gilded_rose4.update_quality)[0].quality).to eq(0)
+    end
   end
 
-  it 'check that update_quality method is working' do
-    expect((gilded_rose.update_quality)[0].sell_in).to eq(19)
-  end
+  describe 'further tests: ' do
+    it 'check when quality is positiv' do
+      expect((gilded_rose5_positiv1.update_quality)[0].quality).to eq(16)
+      expect((gilded_rose5_positiv1.update_quality)[1].quality).to eq(10)
+      expect((gilded_rose5_positiv1.update_quality)[2].quality).to eq(21)
+      expect((gilded_rose5_positiv1.update_quality)[3].quality).to eq(10)
+    end
 
-  it 'check that sell_in date does not increase' do
-    expect((gilded_rose.update_quality)[1].sell_in).not_to eq(16)
-  end
+    # it 'check when quality is positiv(second case for Aged Brie)' do
+    #   expect((gilded_rose5_positiv2.update_quality)[0].quality).to eq(11)
+    # end
+    #
+    # it 'check when quality is positiv(third case for Aged Brie)' do
+    #   expect((gilded_rose5_positiv3.update_quality)[0].quality).to eq(5)
+    # end
 
-  it 'check that quality value equals the right value' do
-    expect((gilded_rose.update_quality)[0].quality).to eq(16)
-  end
-
-  it 'check that quality value equals the right value(second example)' do
-    expect((gilded_rose2.update_quality)[0].quality).to eq(15)
-  end
-
-  it 'check that quality value does not equal the wrong value' do
-    expect((gilded_rose.update_quality)[0].quality).not_to eq(15)
-  end
-
-  it 'check that quality value does not go more than 50' do
-    expect((gilded_rose3.update_quality)[0].quality).to eq(50)
-  end
-
-  it 'check that quality value does not go negative' do
-    expect((gilded_rose4.update_quality)[0].quality).to eq(0)
+    it 'check when quality is positiv(fourth case for Aged Brie)' do
+      expect((gilded_rose5_positiv4.update_quality)[0].quality).to eq(0)
+    end
   end
 end
--- ruby/spec/web_helpers.rb
+++ ruby/spec/web_helpers.rb
@@ -25,3 +25,37 @@ def gilded_rose4
   gilded_updated = gilded_rose.update_quality
   GildedRose.new(gilded_updated)
 end
+
+def gilded_rose5_positiv1
+  items1 = Item.new('Aged Brie', 20, 15)
+  items2 = Item.new('Sulfuras, Hand of Ragnaros', 15, 10)
+  items3 = Item.new('Backstage passes to a TAFKAL80ETC concert', 14, 20)
+  items4 = Item.new('Elixir of the Mongoose', 10, 11)
+  GildedRose.new([items1,items2,items3,items4])
+end
+
+def gilded_rose5_positiv2
+  items1 = Item.new('Aged Brie', 9, 9)
+  items2 = Item.new('Elixir of the Mongoose', 10, 11)
+  GildedRose.new([items1,items2])
+end
+
+def gilded_rose5_positiv3
+  items1 = Item.new('Aged Brie', 5, 2)
+  items2 = Item.new('Elixir of the Mongoose', 10, 11)
+  GildedRose.new([items1,items2])
+end
+
+def gilded_rose5_positiv4
+  items1 = Item.new('Aged Brie', 15, -1)
+  items2 = Item.new('Elixir of the Mongoose', 10, 11)
+  GildedRose.new([items1,items2])
+end
+
+def gilded_rose5_negativ
+  items1 = Item.new('Aged Brie', 20, -3)
+  items2 = Item.new('Sulfuras, Hand of Ragnaros', 15, -4)
+  items3 = Item.new('Backstage passes to a TAFKAL80ETC concert', 14, -2)
+  items4 = Item.new('Elixir of the Mongoose', 10, -2)
+  GildedRose.new([items1,items2,items3,items4])
+end
