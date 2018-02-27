2a358bc1481fe43b0fa8c81dd290c7e4bd391f4b
Alexy <alexandra.fina@yahoo.com>
2018-02-27 14:02:46 +0000

    Test for quality value to not be negative or more than 50

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -28,10 +28,35 @@ describe GildedRose do
     expect((gilded_rose.update_quality)[0].quality).to eq(16)
   end
 
+  it 'check that quality value equals the right value(second example)' do
+    items = Item.new('Sulfuras, Hand of Ragnaros', 20, 15)
+    items2 = Item.new('Aged Brie', 15, 10)
+    gilded_rose = GildedRose.new([items,items2])
+    expect((gilded_rose.update_quality)[0].quality).to eq(15)
+  end
+
   it 'check that quality value does not equal the wrong value' do
     items = Item.new('Aged Brie', 20, 15)
     items2 = Item.new('Aged Brie', 15, 10)
     gilded_rose = GildedRose.new([items,items2])
     expect((gilded_rose.update_quality)[0].quality).not_to eq(15)
+  end
+
+  it 'check that quality value does not go more than 50' do
+    items = Item.new('Aged Brie', 20, 49)
+    items2 = Item.new('Aged Brie', 15, 10)
+    gilded_rose = GildedRose.new([items,items2])
+    gilded_updated = gilded_rose.update_quality
+    gilded_updated2 = GildedRose.new(gilded_updated)
+    expect((gilded_updated2.update_quality)[0].quality).to eq(50)
+  end
+
+  it 'check that quality value does not go negative' do
+    items = Item.new('Elixir of the Mongoose', 20, 1)
+    items2 = Item.new('Aged Brie', 15, 10)
+    gilded_rose = GildedRose.new([items,items2])
+    gilded_updated = gilded_rose.update_quality
+    gilded_updated2 = GildedRose.new(gilded_updated)
+    expect((gilded_updated2.update_quality)[0].quality).to eq(0)
   end
 end
