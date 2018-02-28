3cdb555f72421dd09621344e1a32dc51d65a110e
Alexy <alexandra.fina@yahoo.com>
2018-02-28 11:44:51 +0000

    Refactor tests and refactor code

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -45,16 +45,17 @@ describe GildedRose do
       expect((gilded_rose5_positiv1.update_quality)[3].quality).to eq(10)
     end
 
-    # it 'check when quality is positiv(second case for Aged Brie)' do
-    #   expect((gilded_rose5_positiv2.update_quality)[0].quality).to eq(11)
-    # end
-    #
-    # it 'check when quality is positiv(third case for Aged Brie)' do
-    #   expect((gilded_rose5_positiv3.update_quality)[0].quality).to eq(5)
-    # end
+    it 'check when quality is positiv(second case for Aged Brie)' do
+      expect((gilded_rose5_positiv2.update_quality)[0].quality).to eq(11)
+    end
+
+    it 'check when quality is positiv(third case for Aged Brie)' do
+      expect((gilded_rose5_positiv3.update_quality)[0].quality).to eq(5)
+    end
 
     it 'check when quality is positiv(fourth case for Aged Brie)' do
       expect((gilded_rose5_positiv4.update_quality)[0].quality).to eq(0)
     end
+
   end
 end
